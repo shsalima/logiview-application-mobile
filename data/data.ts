f3cd30772d86c0c519e57b0e29057391e87bcb93
salima <sahisalima77@gmail.com>
2026-08-06 13:58:12 +0200

    ajouter data dans le fichier data.ts

---
+++ data/data.ts
@@ -0,0 +1,32 @@
+export const parcels = [
+  {
+    id: 1,
+    name: "Colis #001",
+    status: "En livraison",
+    destination: "Casablanca",
+    date: "06/08/2026",
+  },
+  {
+    id: 2,
+    name: "Colis #002",
+    status: "Livré",
+    destination: "Rabat",
+    date: "05/08/2026",
+  },
+];
+
+
+export const vehicles = [
+  {
+    id:1,
+    name:"Camion 01",
+    driver:"Ahmed",
+    status:"Disponible"
+  },
+  {
+    id:2,
+    name:"Camion 02",
+    driver:"Youssef",
+    status:"En route"
+  }
+];
